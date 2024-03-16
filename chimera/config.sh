c2a86628b2afe05a35cc7a03868ad404a4478569
chloebessiere <chloe.bessiere@inserm.fr>
2024-03-16 16:19:54 +0100

    chimera : update chimera script

---
+++ chimera/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initialize our variables :
+
+## annotation files
+genome="/scratch/projects/chloe/CCLE/hg38.fa" #reference genome fasta file
+exons="/scratch/projects/chloe/CCLE/gencode.v42.annotation_exons.bed" #bed file of the exons from gencode
+prime3="./gencode/gencode.v42.annotation_exons-3prime.bed" #bedfile of the 3' end of exons generated from the exons bed file
+prime5="./gencode/gencode.v42.annotation_exons-5prime.bed" #bedfile of the 5' end of exons generated from the exons bed file
+
+## rdeer configuration
+server="ella"
+index="CCLE-1019-cut-disk"
--- chimera/depmap_fusion_to_bed.sh
+++ chimera/depmap_fusion_to_bed.sh
@@ -76,17 +76,7 @@ fi
 # MAIN
 #================================================================
 
-# Initialize our own variables :
-
-## annotation files
-genome="/scratch/projects/chloe/CCLE/hg38.fa" #reference genome fasta file
-exons="/scratch/projects/chloe/CCLE/gencode.v42.annotation_exons.bed" #bed file of the exons from gencode
-prime3="./gencode/gencode.v42.annotation_exons-3prime.bed" #bedfile of the 3' end of exons generated from the exons bed file
-prime5="./gencode/gencode.v42.annotation_exons-5prime.bed" #bedfile of the 5' end of exons generated from the exons bed file
-## rdeer configuration
-server="ella"
-index="CCLE-1019-cut-disk"
-
+source config.sh
 
 # All our variables 
 
